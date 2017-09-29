e1af3dd8faf3dd821a4891c537185b0f0e8116e7
Friedrich Weber <fweber@posteo.de>
2017-09-29 21:30:11 +0200

    Pool approach

--- read.py
+++ read.py
@@ -1,4 +1,4 @@
-import re
+import re, itertools
 from pprint import pprint
 from collections import namedtuple, defaultdict
 
@@ -27,10 +27,18 @@ with open('result.txt', 'r') as f:
         match = PATTERN.match(line.strip()).groupdict()
         ctx = Context(parse_trace(match['trace']), match['type'])
         traces[ctx] = int(match['activations'])
+#        shorter = ctx
+#        while True:
+#            shorter = Context(shorter.trace[2:], ctx.java_type)
+#            if len(shorter.trace) <= 1:
+#                break
+#            traces[shorter] = int(match['activations'])
 
 def find_superinstruction(traces, candidate):
     child_class = candidate.trace[-1]
     child_index = candidate.trace[-2]
+    if candidate.trace[-3] == 'SequenceNode':
+        return {}
     alternatives = defaultdict(dict)
     for other in traces:
         if (other.trace[:-2] == candidate.trace[:-2]
@@ -38,16 +46,24 @@ def find_superinstruction(traces, candidate):
             alternatives[other.trace[-2]][other] = traces[other]
     # construct alternatives along with their activation counts
     alternatives[child_index][candidate] = traces[candidate]
-    superinstruction = []
+    constructions = []
     activations = 0
     for idx in sorted(alternatives.keys()):
-        choice = max(alternatives[idx], key=alternatives[idx].get)
-        superinstruction.append(choice)
-        activations += alternatives[idx][choice]
-    return superinstruction, activations
+        construction = list(alternatives[idx].items())
+        constructions.append(construction)
+    pool = {}
+    for combination in itertools.product(*constructions):
+        superinstruction = [x[0] for x in combination]
+        activations = [x[1] for x in combination]
+        pool[tuple(superinstruction)] = sum(activations)
+    return pool
 
 sorted_traces = sorted(traces.keys(), key=traces.get, reverse=True)
+pool = {}
 for t in sorted_traces[:20]:
-    print(t, traces[t])
-    pprint(find_superinstruction(traces, t))
-    print()
+    pool.update(find_superinstruction(traces, t))
+
+top10 = sorted(pool.keys(), key=pool.get, reverse=True)
+for t in top10[:10]:
+    pprint(t)
+    print(pool[t])
